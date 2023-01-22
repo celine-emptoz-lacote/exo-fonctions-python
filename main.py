3f6ac12eccfbfc4232a547202ec5cb348ed1ced6
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2023-01-22 20:49:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+# def welcome():
+#     print('Welcome')
+
+# welcome()
+# welcome()
+# welcome()
+# welcome()
+
+# nombre = 2
+
+# def add_one(nombre):
+#     nombre += 1
+#     return nombre
+
+# print(add_one(nombre))
+# print(add_one(0))
+# print(add_one(15))
+
+# def max(a,b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+
+# num1 = int(input('Entrez la premiere valeur '))
+# num2 = int(input('Entrez la seconde valeur '))
+
+# resultat = max(num1,num2)
+
+# print(resultat)
+
+
+# resultat1 = 12 + 2
+# print(resultat1)
+
+# resultat2 = 6 + 15
+# print(resultat2)
+
+# resultat3 = 1 + 1
+# print(resultat3)
+
+def addition(a,b):
+    resultat = a + b
+    print(resultat)
+
+addition(12,2)
+addition(6,15)
+addition(1,1)
